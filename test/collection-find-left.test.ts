bfbc7b9ce0fc577cdedc337f4f516b165c64edf3
Crinax <agreedeth@gmail.com>
2022-06-20 12:57:33 +0000

    chore: testing

--- test/collection-find-left.test.ts
+++ test/collection-find-left.test.ts
@@ -1,23 +1,19 @@
+import { min } from "../lib/operators/find/min.operator";
+import { gt } from "../lib/operators/logic/gt.operator";
 import { Collection } from "../lib/collection/collection";
-import { even } from "../lib/operators/math/even.operator";
-import { mod } from "../lib/operators/math/mod.operator";
-import { intersection } from "../lib/operators/logic/intersection.operator";
 import { ICollection } from "../lib/common/interfaces/collection.interface";
-import { reverse } from "../lib/operators/map";
 import { log } from "../lib/operators/tap";
 
 const collection = Collection.create<number>(
-  ...Array.from({ length: 2000 }, (_, k) => k),
+  ...Array.from({ length: 21 }, (_, k) => k)
 );
-const square = (num: number) => num ** 2;
-const cube = (num: number) => num ** 3;
+
 let result: ICollection<number>;
 
 console.time();
 
-result = collection
-  .map(square)
-  .intersection(collection.map(cube));
+result = collection.filter(gt(5));
+collection.findLeft(min);
 
 console.timeEnd();
 
