628d925fed543ed53c8468ba5f7e324217488d37
Crinax <agreedeth@gmail.com>
2022-06-20 22:19:46 +0300

    feat(operators): add and operator
    get a collection of all elements for which all logical functions return true

---
+++ lib/operators/logic/and.operator.ts
@@ -0,0 +1,8 @@
+import { ICollection } from "../../common/interfaces/collection.interface";
+import { LikeLogicOperator } from "../../common/types/like-logic-operator.operator";
+
+export const and = <T>(...args: LikeLogicOperator<T>[]) => (
+  element: T,
+  index: number,
+  collection: ICollection<T>,
+) => args.every((fn) => fn(element, index, collection.clone()));
