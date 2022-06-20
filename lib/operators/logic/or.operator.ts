1a4ca6e50ad56d74af62d1b6ec635c0adf0fd811
Crinax <agreedeth@gmail.com>
2022-06-20 22:23:07 +0300

    feat(operators): add or operator
    get a collection of all elements for which
    at least one of logical functions return true

---
+++ lib/operators/logic/or.operator.ts
@@ -0,0 +1,8 @@
+import { ICollection } from "../../common/interfaces/collection.interface";
+import { LikeLogicOperator } from "../../common/types/like-logic-operator.operator";
+
+export const or = <T>(...args: LikeLogicOperator<T>[]) => (
+  element: T,
+  index: number,
+  collection: ICollection<T>,
+) => args.some((fn) => fn(element, index, collection.clone()));
