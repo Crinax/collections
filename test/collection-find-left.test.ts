c8df1268914de6c6c55881c0f5fc1578d9f176c3
Crinax <agreedeth@gmail.com>
2022-06-20 22:25:09 +0300

    test: some testing

--- test/collection-find-left.test.ts
+++ test/collection-find-left.test.ts
@@ -1,8 +1,12 @@
 import { min } from "../lib/operators/find/min.operator";
+import { max } from "../lib/operators/find/max.operator";
 import { gt } from "../lib/operators/logic/gt.operator";
+import { lt } from "../lib/operators/logic/lt.operator";
 import { Collection } from "../lib/collection/collection";
 import { ICollection } from "../lib/common/interfaces/collection.interface";
 import { log } from "../lib/operators/tap";
+import { and } from "../lib/operators/logic/and.operator";
+import { or } from "../lib/operators/logic/or.operator";
 
 const collection = Collection.create<number>(
   ...Array.from({ length: 21 }, (_, k) => k)
@@ -12,8 +16,9 @@ let result: ICollection<number>;
 
 console.time();
 
-result = collection.filter(gt(5));
-collection.findLeft(min);
+result = collection
+  .filter(or(min, gt(5)))
+  .take(1);
 
 console.timeEnd();
 
