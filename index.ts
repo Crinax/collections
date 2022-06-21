dcac2202d4d2201aa523a244503f9972c916b0fc
Crinax <agreedeth@gmail.com>
2022-06-21 20:14:01 +0300

    fix: fix export

---
+++ index.ts
@@ -0,0 +1,3 @@
+export * from "./lib/collection";
+export * from "./lib/common";
+export * from "./lib/operators";
---
+++ lib/operators/index.ts
@@ -0,0 +1,7 @@
+export * from "./filter";
+export * from "./find";
+export * from "./logic";
+export * from "./map";
+export * from "./math";
+export * from "./tap";
+export * from "./unknown";
